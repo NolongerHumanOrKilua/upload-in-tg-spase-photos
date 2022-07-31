a38019233fcac30a077aa517669105c065857db8
NolongerHumanOrKilua <108425756+NolongerHumanOrKilua@users.noreply.github.com>
2022-07-31 21:18:54 +0300

    Create epic_photo.py

---
+++ epic_photo.py
@@ -0,0 +1,31 @@
+import requests
+import datetime
+
+def epic_photo(epic_image):
+    i = 0
+    for photo in epic_image:
+      date = photo["date"]
+      date = date.split(maxsplit=1)
+      date = date[0]
+      image = photo["image"]
+      photo_date = datetime.date.fromisoformat(f'{date}')
+      photo_date = photo_date.strftime("%Y/%m/%d")
+      api_key="xrORzILSdTVL4akZk0CUjPvzVNQq7mhyaTW4eyBS"
+      url = f"https://api.nasa.gov/EPIC/archive/natural/{photo_date}/png/{image}.png?api_key={api_key}"
+      filename = f'images_epic/epic{i}.jpg'
+      response = requests.get(url)
+      response.raise_for_status()
+      with open(filename, 'wb') as f:
+        f.write(response.content)
+      i +=1  
+
+
+def epic_image():
+    api_key = "xrORzILSdTVL4akZk0CUjPvzVNQq7mhyaTW4eyBS"
+    url = f"https://api.nasa.gov/EPIC/api/natural/images?api_key={api_key}"
+    response = requests.get(url)
+    response.raise_for_status()    
+    return response.json()
+
+epic_image = epic_image()
+epic_photo(epic_image)    

f5334299a9f8d85cf60b874bcfebead35a02743b
NolongerHumanOrKilua <108425756+NolongerHumanOrKilua@users.noreply.github.com>
2022-08-01 10:58:23 +0300

    Update fetch_spacex_last_launch.py

--- fetch_spacex_last_launch.py
+++ fetch_spacex_last_launch.py
@@ -1,22 +1,26 @@
 import requests
 import datetime
+import argparse
+
 
 def images():
-  id = input("Введите id запуска или  введите 6243aec2af52800c6e91925d: ")
-  url = f"https://api.spacexdata.com/v5/launches/{id}"
-  response = requests.get(url) 
-  response.raise_for_status()
-  return response.json()["links"]['flickr']['original']
+    id = "5eb87d46ffd86e000604b388"
+    url = f"https://api.spacexdata.com/v5/launches/{id}"
+    response = requests.get(url)
+    response.raise_for_status()
+    return response.json()["links"]['flickr']['original']
+
 
 def fetch_spacex_last_launch(images):
-  i = 0
-  for image in images:
-    filename = f'images/hubble{i}.jpeg'
-    response = requests.get(image)
-    response.raise_for_status()
-    with open(filename, 'wb') as f:
-      f.write(response.content)
-    i +=1  
+    i = 0
+    for image in images:
+        filename = f'images/hubble{i}.jpeg'
+        response = requests.get(image)
+        response.raise_for_status()
+        with open(filename, 'wb') as f:
+            f.write(response.content)
+        i += 1
 
-images =  images() 
+
+images = images()
 fetch_spacex_last_launch(images)

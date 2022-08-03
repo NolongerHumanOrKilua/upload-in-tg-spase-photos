ed16d7f685e5965fde9507cb1a7811b44c389189
NolongerHumanOrKilua <108425756+NolongerHumanOrKilua@users.noreply.github.com>
2022-08-03 21:49:07 +0300

    Update epic_photo.py

--- epic_photo.py
+++ epic_photo.py
@@ -1,7 +1,10 @@
 import os
 import requests
 import datetime
-api_key_nasa = os.environ['API_KEY']
+import utils
+from dotenv import load_dotenv
+load_dotenv()
+api_key = os.environ['NASA_API_KEY']
 
 def epic_photo(epic_image):
     i = 0
@@ -15,15 +18,11 @@ def epic_photo(epic_image):
       api_key="xrORzILSdTVL4akZk0CUjPvzVNQq7mhyaTW4eyBS"
       url = f"https://api.nasa.gov/EPIC/archive/natural/{photo_date}/png/{image}.png?api_key={api_key}"
       filename = f'images/epic{i}.jpg'
-      response = requests.get(url)
-      response.raise_for_status()
-      with open(filename, 'wb') as f:
-        f.write(response.content)
-      i +=1  
+      utils.save_photo(filename, url)
+      i+=1
 
 
 def epic_image():
-    api_key = api_key_nasa
     url = f"https://api.nasa.gov/EPIC/api/natural/images?api_key={api_key}"
     response = requests.get(url)
     response.raise_for_status()    

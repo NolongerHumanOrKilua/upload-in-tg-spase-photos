50bae43677e4112ff755e3948261176d4750bd6c
NolongerHumanOrKilua <108425756+NolongerHumanOrKilua@users.noreply.github.com>
2022-08-06 15:18:33 +0300

    Update to_tg_bot.py

--- to_tg_bot.py
+++ to_tg_bot.py
@@ -1,20 +1,21 @@
+from importlib.resources import path
 import os
 from turtle import delay
 import telegram
-import os   
+import os
 import time
 
-chat = input()
+chat = os.environ["CHAT_ID"]
 tg_token = os.environ['TG_TOKEN']
 bot = telegram.Bot(token=tg_token)
 delay = 14400
+path = "images"
 while True:
-    for dir_names, files in os.walk('images/'):
+    for dir_names, files in os.walk(path):
         for file in files:
             with open(os.path.join(file), "rb") as file:
                 bot.send_document(
-                        chat_id=chat,
-                        document=file
-                        )
+                    chat_id=chat,
+                    document=file
+                )
             time.sleep(delay)
-      

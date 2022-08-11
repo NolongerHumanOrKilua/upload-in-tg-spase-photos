fd9ef13637d86c4bedbab673923d9e521732ee08
NolongerHumanOrKilua <108425756+NolongerHumanOrKilua@users.noreply.github.com>
2022-08-11 16:44:12 +0300

    Create last_launch.py

---
+++ last_launch.py
@@ -0,0 +1,31 @@
+from email.mime import image
+import requests
+import datetime
+import argparse
+import utils
+import os
+from pathlib import Path
+
+
+def get_images():
+    id = "5eb87d46ffd86e000604b388"
+    url = f"https://api.spacexdata.com/v5/launches/{id}"
+    response = requests.get(url)
+    response.raise_for_status()
+    return response.json()["links"]['flickr']['original']
+
+
+def get_fetch_spacex_last_launch(images, path):
+    for photo_num, image in enumerate(images):
+        filename = os.path.join(path, f"spacex{photo_num}.jpg")
+        utils.save_photo(filename, image, params=None)
+
+
+def main():
+    path = os.path.join(os.getcwd(), "images")
+    images = get_images()
+    get_fetch_spacex_last_launch(images)
+
+
+if __name__ == "__main__":
+    main()

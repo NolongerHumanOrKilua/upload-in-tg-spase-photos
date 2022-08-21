37f58aa274da1e706520b687b6bc65876a25b0ea
NolongerHumanOrKilua <108425756+NolongerHumanOrKilua@users.noreply.github.com>
2022-08-21 22:41:25 +0300

    Update last_launch.py

--- last_launch.py
+++ last_launch.py
@@ -9,8 +9,8 @@ from pathlib import Path
 import argparse
 
 
-def get_images(input_id):
-    url = f"https://api.spacexdata.com/v5/launches/{input_id}" 
+def get_images(launch_id):
+    url = f"https://api.spacexdata.com/v5/launches/{launch_id}"
     response = requests.get(url)
     response.raise_for_status()
     return response.json()["links"]['flickr']['original']
@@ -23,12 +23,10 @@ def get_fetch_spacex_last_launch(images, path):
 
 
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("input_id", type=str, default="latest")
-    args = parser.parse_args()
     path = os.path.join(os.getcwd(), "images")
     os.makedirs(path, exist_ok=True)
-    images = get_images(args.input_id)
+    launch_id = "latest"
+    images = get_images(launch_id)
     get_fetch_spacex_last_launch(images, path)
 
 

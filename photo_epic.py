cd419d4a6ecda7ff74382cbc6d297fff78fe8204
NolongerHumanOrKilua <108425756+NolongerHumanOrKilua@users.noreply.github.com>
2022-08-21 22:41:19 +0300

    Update photo_epic.py

--- photo_epic.py
+++ photo_epic.py
@@ -7,22 +7,20 @@ from _collections_abc import MutableMapping
 from pathlib import Path
 
 
-
 def get_epic_photo(epic_image, api_key, path):
     for photo_number, photo in enumerate(epic_image):
-        date = photo["date"]
+        date = photo["date"].split(maxsplit=1)[0]
         image = photo["image"]
-        photo_date = datetime.date.fromisoformat(date[0])
+        photo_date = datetime.date.fromisoformat(date)
         photo_date = photo_date.strftime("%Y/%m/%d")
         params = {"api_key": api_key}
         url = f"https://api.nasa.gov/EPIC/archive/natural/{photo_date}/png/{image}.png"
         filename = os.path.join(path, f"epic{photo_number}.jpg")
         utils.save_photo(filename, url, params)
- 
+
 
 def get_epic_image(api_key):
-    key = api_key
-    params = {"api_key": key}
+    params = {"api_key": api_key}
     url = f"https://api.nasa.gov/EPIC/api/natural/images"
     response = requests.get(url, params=params)
     response.raise_for_status()

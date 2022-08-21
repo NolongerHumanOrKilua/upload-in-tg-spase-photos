2efe34a1d51193a81fb2f292077f779991f9c00b
NolongerHumanOrKilua <108425756+NolongerHumanOrKilua@users.noreply.github.com>
2022-08-21 22:41:22 +0300

    Update nasa_photo.py

--- nasa_photo.py
+++ nasa_photo.py
@@ -5,35 +5,30 @@ import utils
 from dotenv import load_dotenv
 from pathlib import Path
 
-def get_nasa_photo(api_key):
-  start_date = datetime.date.fromisoformat('2022-06-29')
-  end_date = datetime.date.fromisoformat('2022-07-29')
-  api_key=api_key
-  url = "https://api.nasa.gov/planetary/apod"
-  parameters = { "start_date": f"{start_date}",
-        "end_date": f"{end_date}", "api_key": f"{api_key}"}
-  response = requests.get(url, params=parameters)
-  response.raise_for_status()  
-  return response.json()
-  
-def save_nasa_ph(nasa_photo, path):
-  for photo_number, photo in enumerate(nasa_photo):
-    url = photo["url"]
-    filename = os.path.join(path, f"nasa{photo_number}.jpg")
-    utils.save_photo(filename, url)
 
+def get_nasa_photo(api_key):
+        start_date = datetime.date.fromisoformat('2022-06-29')
+        end_date = datetime.date.fromisoformat('2022-07-29')
+        url = "https://api.nasa.gov/planetary/apod"
+        params = {"start_date": f"{start_date}", "end_date": f"{end_date}", "api_key": f"{api_key}"}
+        response = requests.get(url, params=params)
+        response.raise_for_status()
+        return response.json()
+
+
+def save_nasa_ph(nasa_photo, path):
+    for photo_number, photo in enumerate(nasa_photo):
+        url = photo["url"]
+        filename = os.path.join(path, f"nasa{photo_number}.jpg")
+        utils.save_photo(filename, url)
 
 
 def main():
     load_dotenv()
     api_key = os.environ['NASA_API_KEY']
     path = os.path.join(os.getcwd(), "images")
-    try:
-        nasa_photo = get_nasa_photo(api_key)
-        save_nasa_ph(nasa_photo)  
-    except:
-        return print("error")
-
+    nasa_photo = get_nasa_photo(api_key)
+    save_nasa_ph(nasa_photo, path)
 
 if __name__ == "__main__":
     main()

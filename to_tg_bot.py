a5e104332fd7fad8117bcd4881f5b78985be8003
NolongerHumanOrKilua <108425756+NolongerHumanOrKilua@users.noreply.github.com>
2022-08-23 23:26:08 +0300

    Update to_tg_bot.py

--- to_tg_bot.py
+++ to_tg_bot.py
@@ -1,21 +1,23 @@
 from importlib.resources import path
 import os
-from turtle import delay
 import telegram
-import os
 import time
+from dotenv import load_dotenv
 
+load_dotenv()
 chat = os.environ["TG_CHAT_ID"]
 tg_token = os.environ['TG_TOKEN']
-bot = telegram.Bot(token=tg_token)
 delay = 14400
 path = "images"
-while True:
-    for dir_names, files in os.walk(path):
+try:
+    bot = telegram.Bot(token=tg_token)
+    for root, dir_names, files in os.walk(path):
         for file in files:
-            with open(os.path.join(file), "rb") as file:
+            with open(os.path.join(root, file), "rb") as file:
                 bot.send_document(
                     chat_id=chat,
                     document=file
                 )
             time.sleep(delay)
+except telegram.error.NetworkError:
+    print("network error")             

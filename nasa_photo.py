ef3289d57f2f5a948a2c13c656f61052e36af950
NolongerHumanOrKilua <108425756+NolongerHumanOrKilua@users.noreply.github.com>
2022-08-23 23:26:01 +0300

    Update nasa_photo.py

--- nasa_photo.py
+++ nasa_photo.py
@@ -4,31 +4,38 @@ import datetime
 import utils
 from dotenv import load_dotenv
 from pathlib import Path
+import argparse
+from datetime import datetime, timedelta
 
-
-def get_nasa_photo(api_key):
-        start_date = datetime.date.fromisoformat('2022-06-29')
-        end_date = datetime.date.fromisoformat('2022-07-29')
+def get_nasa_photo(api_key, start_date, end_date):
         url = "https://api.nasa.gov/planetary/apod"
-        params = {"start_date": f"{start_date}", "end_date": f"{end_date}", "api_key": f"{api_key}"}
+        params = {"start_date": start_date, "end_date": end_date, "api_key": api_key}
         response = requests.get(url, params=params)
         response.raise_for_status()
         return response.json()
 
 
-def save_nasa_ph(nasa_photo, path):
+def save_nasa_photos(nasa_photo, path):
     for photo_number, photo in enumerate(nasa_photo):
-        url = photo["url"]
-        filename = os.path.join(path, f"nasa{photo_number}.jpg")
-        utils.save_photo(filename, url)
+        if photo["media_type"] == "image":
+            url = photo["url"]
+            filename = os.path.join(path, f"nasa{photo_number}.jpg")
+            utils.save_photo(filename, url)
 
 
 def main():
     load_dotenv()
     api_key = os.environ['NASA_API_KEY']
     path = os.path.join(os.getcwd(), "images")
-    nasa_photo = get_nasa_photo(api_key)
-    save_nasa_ph(nasa_photo, path)
+    os.makedirs(path, exist_ok=True)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-end_date", default=datetime.now())
+    parser.add_argument("-start_date", default=datetime.now()-timedelta(days=30))
+    args = parser.parse_args()
+    start_date = args.start_date.strftime("%Y-%m-%d")
+    end_date = args.end_date.strftime("%Y-%m-%d")
+    nasa_photo = get_nasa_photo(api_key,start_date, end_date)
+    save_nasa_photos(nasa_photo, path)
 
 if __name__ == "__main__":
     main()

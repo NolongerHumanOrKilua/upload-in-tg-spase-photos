7f4c9489062b1658d87aab4017663f45fad33a2b
NolongerHumanOrKilua <108425756+NolongerHumanOrKilua@users.noreply.github.com>
2022-08-29 22:35:40 +0300

    Update to_tg_bot.py

--- to_tg_bot.py
+++ to_tg_bot.py
@@ -1,5 +1,5 @@
-from asyncio import trsock
 import os
+from xml.dom.minidom import Document
 import telegram
 import time
 from dotenv import load_dotenv
@@ -12,16 +12,17 @@ delay = 1
 path = "images"
 files = os.listdir(path=path)
 
-try:
-    while True:
-        bot = telegram.Bot(token=tg_token)
-        for root, dir_names, files in os.walk(path):
-            random.shuffle(files)
-            for file in files:
-                with open(os.path.join(root, file), "rb") as file:
-                    bot.send_document(chat_id=chat, document=file)
-                time.sleep(delay)
+while True:
+    try:
+            bot = telegram.Bot(token=tg_token)
+            for root, dir_names, files in os.walk(path):
+                random.shuffle(files)
+                for file in files:
+                    with open(os.path.join(root, file), "rb") as file:
+                        bot.send_document(chat_id=chat, document=file)
+                    time.sleep(delay)
+    except telegram.error.NetworkError:
+            time.sleep(10)
 
 
-except telegram.error.NetworkError:
-    print("network error")
+        

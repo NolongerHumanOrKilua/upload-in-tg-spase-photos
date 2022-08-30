2c984b5d4686b34565398e94c08c273efcab5a8f
NolongerHumanOrKilua <108425756+NolongerHumanOrKilua@users.noreply.github.com>
2022-08-30 20:18:19 +0300

    Update nasa_photo.py

--- nasa_photo.py
+++ nasa_photo.py
@@ -1,18 +1,20 @@
-import os
-import requests
-import datetime
-import utils
-from dotenv import load_dotenv
 import argparse
+import datetime
+import os
 from datetime import datetime, timedelta
+
+import requests
+from dotenv import load_dotenv
+
+import utils
 
 
 def get_nasa_photo(api_key, start_date, end_date):
-        url = "https://api.nasa.gov/planetary/apod"
-        params = {"start_date": start_date, "end_date": end_date, "api_key": api_key}
-        response = requests.get(url, params=params)
-        response.raise_for_status()
-        return response.json()
+    url = "https://api.nasa.gov/planetary/apod"
+    params = {"start_date": start_date, "end_date": end_date, "api_key": api_key}
+    response = requests.get(url, params=params)
+    response.raise_for_status()
+    return response.json()
 
 
 def save_nasa_photos(nasa_photo, path):
@@ -25,17 +27,18 @@ def save_nasa_photos(nasa_photo, path):
 
 def main():
     load_dotenv()
-    api_key = os.environ['NASA_API_KEY']
+    api_key = os.environ["NASA_API_KEY"]
     path = os.path.join(os.getcwd(), "images")
     os.makedirs(path, exist_ok=True)
     parser = argparse.ArgumentParser()
     parser.add_argument("-end_date", default=datetime.now().strftime("%Y-%m-%d"))
-    parser.add_argument("-start_date", default=(datetime.now()-timedelta(days=30)).strftime("%Y-%m-%d"))
+    parser.add_argument("-start_date", default=(datetime.now() - timedelta(days=30)).strftime("%Y-%m-%d"))
     args = parser.parse_args()
     start_date = args.start_date
     end_date = args.end_date
     nasa_photo = get_nasa_photo(api_key, start_date, end_date)
     save_nasa_photos(nasa_photo, path)
+
 
 if __name__ == "__main__":
     main()

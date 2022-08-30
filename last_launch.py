8b9a55fce45447f64fce14f4b1e603e18b39db9b
NolongerHumanOrKilua <108425756+NolongerHumanOrKilua@users.noreply.github.com>
2022-08-30 20:18:15 +0300

    Update last_launch.py

--- last_launch.py
+++ last_launch.py
@@ -1,14 +1,16 @@
-import requests
 import argparse
-import utils
 import os
+
+import requests
+
+import utils
 
 
 def get_images(launch_id):
     url = f"https://api.spacexdata.com/v5/launches/{launch_id}"
     response = requests.get(url)
     response.raise_for_status()
-    return response.json()["links"]['flickr']['original']
+    return response.json()["links"]["flickr"]["original"]
 
 
 def get_fetch_spacex_last_launch(images, path):

84c31b97cd04932948ec138e9f83681c4439af3b
NolongerHumanOrKilua <108425756+NolongerHumanOrKilua@users.noreply.github.com>
2022-08-30 20:18:36 +0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -4,5 +4,5 @@ import requests
 def save_photo(filename, image, params=None):
     response = requests.get(image, params=params)
     response.raise_for_status()
-    with open(filename, 'wb') as f:
+    with open(filename, "wb") as f:
         f.write(response.content)
